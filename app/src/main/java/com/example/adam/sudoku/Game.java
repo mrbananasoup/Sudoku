3e098ac2a59eb363655d4f8c532d8f4a0cc938c8
Adam Parker <36685382+mrbananasoup@users.noreply.github.com>
2018-02-21 22:43:41 +0000

    Update Game.java

--- app/src/main/java/com/example/adam/sudoku/Game.java
+++ app/src/main/java/com/example/adam/sudoku/Game.java
@@ -1,5 +1,9 @@
 package com.example.adam.sudoku;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +16,9 @@ public class Game extends AppCompatActivity {
 
     final SudokuBoard mediumGame = new MediumGame();
     TextView[][] cells = new TextView[9][9];
+    View currView = null;
+
+    String currCell = "";
 
     public void startGame(View view)
     {
@@ -42,12 +49,34 @@ public class Game extends AppCompatActivity {
         cells[x][y].setText("" + mediumGame.solutionBoard[x][y].getNumber());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void btnCell(View view)
     {
         String id = view.getResources().getResourceName(view.getId());
         id = id.substring(id.length() - 3, id.length());
+        currCell = id;
         mediumGame.solutionBoard[Integer.parseInt(id.substring(0,1))][Integer.parseInt(id.substring(2,3))].setNumber(0); // TODO test for cell on click remove later
+
+        setSelected(view);
         refreshCells(Integer.parseInt(id.substring(0,1)),Integer.parseInt(id.substring(2,3)));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setSelected(View view)
+    {
+        if(currView != null) currView.setBackgroundTintList(null);
+        if(currView == view)
+        {
+            currView.setBackgroundTintList(null);
+            currView = null;
+            currCell = "";
+        }
+        else
+        {
+            view.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+            currView = view;
+        }
+
     }
 
     @Override
