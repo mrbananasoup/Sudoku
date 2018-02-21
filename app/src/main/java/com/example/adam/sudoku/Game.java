24c91af233548bbfdf69c02f3bd8bb2a20ef56a8
mrbananasouptest <0105(&Git()>
2018-02-21 06:31:32 +0000

    new game class

--- app/src/main/java/com/example/adam/sudoku/Game.java
+++ app/src/main/java/com/example/adam/sudoku/Game.java
@@ -22,6 +22,8 @@ public class Game extends AppCompatActivity {
         //new
         mediumGame.calculateSmall();
 
+        System.out.print("Git Hub Test");
+
         System.out.println(Arrays.toString(mediumGame.gameBoard[0][0].getSmallNumbers()));
 
         final Button button = findViewById(R.id.btnStart);
