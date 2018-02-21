cf45aed2a19c62013da650491707e87a026c537e
mrbananasoup <adamparker1@users.noreply.github.com>
2018-02-21 12:52:18 +0000

    comment remove

--- app/src/main/java/com/example/adam/sudoku/Game.java
+++ app/src/main/java/com/example/adam/sudoku/Game.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -19,7 +20,6 @@ public class Game extends AppCompatActivity {
         mediumGame.createSudoku();
         mediumGame.makeHoles();
         mediumGame.printGame();
-        //new
         mediumGame.calculateSmall();
 
         System.out.println(Arrays.toString(mediumGame.gameBoard[0][0].getSmallNumbers()));
@@ -28,8 +28,8 @@ public class Game extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.game);
 
+                setContentView(R.layout.game);
                 TextView[][] cells = new TextView[9][9];
 
                 for(int i = 0; i < 9; i++)
