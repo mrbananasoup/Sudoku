0d0b813203d16340d4f95d56009337b77c58bab5
mrbananasoup <adamparker1@users.noreply.github.com>
2018-02-21 07:02:42 +0000

    last test 02

--- app/src/main/java/com/example/adam/sudoku/Game.java
+++ app/src/main/java/com/example/adam/sudoku/Game.java
@@ -23,6 +23,7 @@ public class Game extends AppCompatActivity {
         mediumGame.calculateSmall();
 
         System.out.println("This is my last te for this time");
+        System.out.println("This is my last te for this time");
 
         System.out.println(Arrays.toString(mediumGame.gameBoard[0][0].getSmallNumbers()));
 
