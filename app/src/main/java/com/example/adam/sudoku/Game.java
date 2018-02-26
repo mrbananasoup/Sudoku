316e95571a2530f120451fa6183933357cd8baf9
mrbananasoup <adamparker1@hotmail.co.uk>
2018-02-26 12:10:25 +0000

    add to cells and cell restriction added

--- app/src/main/java/com/example/adam/sudoku/Game.java
+++ app/src/main/java/com/example/adam/sudoku/Game.java
@@ -4,12 +4,9 @@ package com.example.adam.sudoku;
  * Created by Adam on 19/02/2018.
  */
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Build;
-import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,22 +17,23 @@ import android.widget.TextView;
 import java.util.Arrays;
 
 public class Game extends AppCompatActivity {
-    SudokuBoard sudokuBoard;
+    // this is a game
+    final SudokuBoard mediumGame = new MediumGame();
     TextView[][] cells = new TextView[9][9];
     View currView = null;
     String currCell = "";
+    // ...
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void startGame(View view)
     {
-        sudokuBoard = initSudokuBoard();
-        sudokuBoard.createSudoku();
-        sudokuBoard.makeHoles();
-        sudokuBoard.printGame();
-        sudokuBoard.calculateSmall();
+        mediumGame.createSudoku();
+        mediumGame.makeHoles();
+        mediumGame.printGame();
+        mediumGame.calculateSmall();
 
         // Just for Test TODO remove later
-        System.out.println(Arrays.toString(sudokuBoard.gameBoard[0][0].getSmallNumbers()));
+        System.out.println(Arrays.toString(mediumGame.gameBoard[0][0].getSmallNumbers()));
 
         setContentView(R.layout.game);
 
@@ -46,21 +44,15 @@ public class Game extends AppCompatActivity {
                 String textID = "Cell" + i + "-" + j;
                 int resID = getResources().getIdentifier(textID, "id", getPackageName());
                 cells[i][j] = ((TextView)findViewById(resID));
-                cells[i][j].setText("" + sudokuBoard.gameBoard[i][j].getNumber());
-                if(sudokuBoard.gameBoard[i][j].getNumber() != 0) cells[i][j].setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+                cells[i][j].setText("" + mediumGame.gameBoard[i][j].getNumber());
+                if(mediumGame.gameBoard[i][j].getNumber() != 0) cells[i][j].setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
             }
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void openSettings(View view) {
-        Intent settingsIntent = new Intent(this, SettingsActivity.class);
-        startActivity(settingsIntent);
-    }
-
     private void refreshCells(int x, int y)
     {
-        cells[x][y].setText("" + sudokuBoard.gameBoard[x][y].getNumber());
+        cells[x][y].setText("" + mediumGame.gameBoard[x][y].getNumber());
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -68,7 +60,7 @@ public class Game extends AppCompatActivity {
     {
         String id = view.getResources().getResourceName(view.getId());
         id = id.substring(id.length() - 3, id.length());
-        if(sudokuBoard.gameBoard[Integer.parseInt(id.substring(0,1))][Integer.parseInt(id.substring(2,3))].getIsEditable())
+        if(mediumGame.gameBoard[Integer.parseInt(id.substring(0,1))][Integer.parseInt(id.substring(2,3))].getIsEditable())
         {
             currCell = id;
             setSelected(view);
@@ -99,7 +91,7 @@ public class Game extends AppCompatActivity {
         if(currCell != "")
         {
             String num = view.getResources().getResourceName(view.getId());
-            sudokuBoard.gameBoard[Integer.parseInt(currCell.substring(0,1))][Integer.parseInt(currCell.substring(2,3))].setNumber(Integer.parseInt(num.substring(num.length() - 1)));
+            mediumGame.gameBoard[Integer.parseInt(currCell.substring(0,1))][Integer.parseInt(currCell.substring(2,3))].setNumber(Integer.parseInt(num.substring(num.length() - 1)));
             refreshCells(Integer.parseInt(currCell.substring(0,1)),Integer.parseInt(currCell.substring(2,3)));
             currCell = "";
             currView.setBackgroundTintList(null);
@@ -111,26 +103,5 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-    }
-
-    protected SudokuBoard initSudokuBoard() {
-        String difficulty = getDifficulty();
-
-        switch(difficulty) {
-            case "easy":
-                return new EasyGame();
-            case "medium":
-                return new MediumGame();
-            case "hard":
-                return new HardGame();
-            default:
-                return new MediumGame();
-        }
-    }
-
-    protected String getDifficulty() {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        return sharedPreferences.getString("pref_difficulty", "");
-
     }
 }
